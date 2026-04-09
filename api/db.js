be2b5b95ae89836f6be89f83a19186f5b6819642
Arianna Tamayo <arianna@marketplaceops.com>
2026-04-09 21:42:31 +0800

    fix ssl

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg');
-
+ 
 const pool = new Pool({
   connectionString: process.env.mopdb_POSTGRES_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
-
+ 
 module.exports = pool;
