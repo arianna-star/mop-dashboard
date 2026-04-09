1f04befe460edf24017ec071620f674e4809e59c
Arianna Tamayo <arianna@marketplaceops.com>
2026-04-09 21:45:34 +0800

    fix db connection string

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg');
- 
+
 const pool = new Pool({
-  connectionString: process.env.mopdb_POSTGRES_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  connectionString: process.env.mopdb_POSTGRES_URL_NON_POOLING,
+  ssl: { rejectUnauthorized: false }
 });
- 
+
 module.exports = pool;
